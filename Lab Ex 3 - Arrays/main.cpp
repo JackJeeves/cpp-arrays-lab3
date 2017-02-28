bbe07b0d7d3f0b7029b87b425abe7a2e0dcf4e1b
Tschech <500138477@fvtc.edu>
2017-02-28 12:01:15 -0600

    Finished the project

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -1,3 +1,7 @@
+//Eli Tshcech
+//Jack Harris
+//Philip Braatz
+
 
 // Lab Exercise 3 - Arrays
 // Instructions:
@@ -16,18 +20,58 @@
 #include <iostream>
 #include <conio.h>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+	vector<float> list;
+	int input = 0;
+	char stayHere = '\0';
+
+	do {
+		cout << "Please enter at least a number: ";
+		cin >> input;
+
+		list.push_back(input);
+
+	} while (list.size() != 5);
+
 	
+	//DISPLAYING THE INFORMATION WE WERE GIVEN
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		cout << "A number is: " << list.at(i) << endl;
+	}
+	cout << endl;
+
+	reverse(list.begin(), list.end());
 	
-	cout << "Please enter at least 5 numbers: ";
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		cout << "A number is: " << list.at(i) << endl;
+	}
+	cout << endl;
 
+	double highest = list.at(0);
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (list.at(i) >= highest)
+			highest = list.at(i);
+	}
+	cout << "The highest number is: " << highest;
+	cout << endl;
 
-
-
+	double lowest = list.at(0);
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (list.at(i) <= lowest)
+			lowest = list.at(i);
+	}
+	cout << "The lowest number is: " << lowest;
+	cout << endl;
 
 	_getch();
 	return 0;
